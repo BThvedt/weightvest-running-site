d9d89e1a6be8790c0be9d6fb87d0510e435b6d48
BThvedt <bthvedt@gmail.com>
2025-09-19 17:28:32 -0500

    Modified the block for dedication

--- web/modules/custom/general/src/Plugin/Block/GuineaPigBlock.php
+++ web/modules/custom/general/src/Plugin/Block/GuineaPigBlock.php
@@ -17,13 +17,16 @@ class GuineaPigBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $module_path = \Drupal::service('extension.list.module')->getPath('general');
+    $image_url = '/' . $module_path . '/images/guinea_pig.jpg';
+
     return [
       '#markup' => '
 				<div class="hover:text-orange">
 					<a class="no-underline" href = "/penny">
 						<div class="footer-guinea-pig block display-flex items-center mt-lg mb-lg">
-							<img class="mr-lg" src="/sites/default/files/guinea_pig.jpg" alt="Penny" />
-							<p class="position-relative bottom-2px">Dedicated to Penny, the Best Guinea Pig</p>
+							<img class="mr-lg" src="' . $image_url . '" alt="Penny the Guinea Pig" />
+							<p class="position-relative bottom-2px">Dedicated to Penny, <br />the Best Guinea Pig</p>
 						</div>
 					</a>
 				</div>
